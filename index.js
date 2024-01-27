19e2203df008bf980c90471f0a42711bd1fbb19e
zeinab <swarsama@hotmail.co.uk>
2024-01-27 22:35:59 +0000

    working on event listeners for input dates

--- index.js
+++ index.js
@@ -3,4 +3,15 @@
 const input = document.querySelectorAll("input");
 const addInput = document.querySelectorAll("span");
 const errorMessage = document.querySelectorAll(".error");
-const redErrorTextAbov = document.querySelectorAll(".red");
+const redErrorTextAbove = document.querySelectorAll(".red");
+const btn = document.querySelector("button");
+
+
+input.forEach((i) => {
+  // console.log(i)
+  btn.addEventListener("submit", (event) => {
+  event.preventDefault()
+  console.log(event);
+  })
+}
+);
