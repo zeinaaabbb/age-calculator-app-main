729f7a5e85b6bbabab5756736d5169e126648507
zeinab <swarsama@hotmail.co.uk>
2024-01-27 22:22:02 +0000

    declaring JS variables

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // console.log("hello");
+
+const input = document.querySelectorAll("input");
+const addInput = document.querySelectorAll("span");
+const errorMessage = document.querySelectorAll(".error");
+const redErrorTextAbov = document.querySelectorAll(".red");
