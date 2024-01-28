6e46bb18b21cb37f01f46f456d3bc79998057fc3
zeinab <swarsama@hotmail.co.uk>
2024-01-28 12:37:38 +0000

    organised declared variables

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
 
 const form = document.querySelector("form");
-const input = document.querySelectorAll("input");
-
+const btn = document.querySelector("button");
 const inputField = document.querySelector(".input-field");
 
-const addInput = document.querySelectorAll("span");
-const errorMessage = document.querySelectorAll(".error");
-const redErrorTextAbove = document.querySelectorAll(".red");
-const btn = document.querySelector("button");
-
+//Input elements
 const day = document.getElementById("input-1");
 const month = document.getElementById("input-2");
 const year = document.getElementById("input-3");
 
+//Output element
+const outputYears = document.getElementById("years");
+const outputMonths = document.getElementById("months");
+const outputDays = document.getElementById("days");
+
+//Error elements below
 const dayEmpty = document.getElementById("hidden-1");
 const monthEmpty = document.getElementById("hidden-2");
 const yearEmpty = document.getElementById("hidden-3");
 
-const error = document.getElementsByTagName("p");
+//Error red above text
+const redErrorDay = document.getElementById("red-d");
+const redErrorMonth = document.getElementById("red-m");
+const redErrorYear = document.getElementById("red-y");
 
 
-// function empty(day, month, year) {
-//   if ((day.value || month.value || year.value) = "") {
-//     error.classList.remove("error");
-//   }
-//   end
-// };
+    function correctDay (day) {
+      const lowerDay  = 1 ;
+      const upperDay = 31 ;
 
-function correctDay (day) {
-  const lowerDay  = 1 ;
-  const upperDay = 31 ;
-
-  if (day > 1 && day <31) {
-    console.log("in range");
-    return day
-  } else {
-    console.log("out of range");
-  }
-};
+      if (day > 1 && day <31) {
+        console.log("in range");
+        return day
+      } else {
+        console.log("out of range");
+      }
+    };
 
 
-form.addEventListener("submit" , (event) => {
-  event.preventDefault()
-  // console.log(day.value);
-  // console.log(month.value);
-  // console.log(year.value);
-  if (correctDay) {
-    dayEmpty.innerText = day.value ;
-  }
+    form.addEventListener("submit" , (event) => {
+      event.preventDefault()
+      console.log(day.value);
+      console.log(month.value);
+      console.log(year.value);
+      console.log(dayEmpty.innerText);
+      dayEmpty.innerText = day.value;
 });
+
+// dayEmpty.innerText = day.value ;
