e42e85f29b4235390dd79b1766e80ca364ddd6df
zeinab <swarsama@hotmail.co.uk>
2024-01-28 14:22:04 +0000

    more changes made

--- index.js
+++ index.js
@@ -14,15 +14,19 @@ const outputMonths = document.getElementById("months");
 const outputDays = document.getElementById("days");
 
 //Error elements below
-const dayEmpty = document.getElementById("hidden-1");
-const monthEmpty = document.getElementById("hidden-2");
-const yearEmpty = document.getElementById("hidden-3");
+const dayField = document.getElementsByClassName("hidden-1");
+const monthField = document.getElementsByClassName("hidden-2");
+const yearField = document.getElementsByClassName("hidden-3");
 
 //Error red above text
-const redErrorDay = document.getElementById("red-d");
-const redErrorMonth = document.getElementById("red-m");
-const redErrorYear = document.getElementById("red-y");
+const redErrorDay = document.getElementsByClassName("red-d");
+const redErrorMonth = document.getElementsByClassName("red-m");
+const redErrorYear = document.getElementsByClassName("red-y");
 
+//label
+const labelDay = document.getElementById("label-d");
+const labelMonth = document.getElementById("label-m");
+const labelYear = document.getElementById("label-y");
 
     function correctDay (day) {
       const lowerDay  = 1 ;
@@ -32,18 +36,20 @@ const redErrorYear = document.getElementById("red-y");
         console.log("in range");
         return day
       } else {
-        console.log("out of range");
+        redErrorDay.classList.remove("red-d");
+        labelDay.style.display = "none";
+        return dayField.innerText
+
       }
     };
 
 
     form.addEventListener("submit" , (event) => {
       event.preventDefault()
-      console.log(day.value);
-      console.log(month.value);
-      console.log(year.value);
-      console.log(dayEmpty.innerText);
-      dayEmpty.innerText = day.value;
+      // console.log(day.value);
+      // console.log(month.value);
+      // console.log(year.value);
+      correctDay(day);
+      // console.log(dayEmpty.innerText);
+      // dayEmpty.innerText = day.value;
 });
-
-// dayEmpty.innerText = day.value ;
